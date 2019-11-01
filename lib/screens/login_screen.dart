6691dc1f65ca58dbd586a86e568a24e57e21da87
nhat tri <nhattri1503@gmail.com>
2019-11-01 20:15:27 +0700

    Login Email + GoogleAuth

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,84 +1,192 @@
+import 'package:dacn/screens/stacked_icons.dart';
+import 'package:dacn/app.dart';
+import 'package:dacn/screens/home_screen.dart' as prefix0;
+import 'package:dacn/screens/dialog/loading_dialog.dart';
+import 'package:dacn/screens/dialog/msg_dilog.dart';
+import 'package:dacn/screens/register_page.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'stacked_icons.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
 
-class LoginPage extends StatelessWidget {
+final GoogleSignIn _googleSignIn = GoogleSignIn(
+  scopes: <String>[
+    'email',
+    'https://www.googleapis.com/auth/contacts.readonly',
+  ],
+);
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+ Future<void> _handleSignIn() async {
+    try {
+      await _googleSignIn.signIn();
+
+      print("this user is signed in" + _googleSignIn.currentUser.displayName);
+
+
+    } catch (error) {
+      print(error);
+    }
+  }
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passController = TextEditingController();
   
+  
+  
+
   @override
   Widget build(BuildContext context) {
     
-SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
-  statusBarColor: Colors.orange, //or set color with: Color(0xFF0000FF)
-));
-    return new Scaffold(
-      appBar: new AppBar(
-        backgroundColor:Colors.transparent,
-          elevation: 0.0,
-          iconTheme: new IconThemeData(color: Color(0xFF5B16D0))),
+    return Scaffold(
+      
       body: Container(
-        width: double.infinity,
-        child: new Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            new StakedIcons(),
-            new Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(top: 8.0, bottom: 80.0),
-                  child: new Text(
-                    "漢字の魂",
-                    style: new TextStyle(fontSize: 30.0),
-                  ),
-                )
-              ],
-            ),
+        padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+        constraints: BoxConstraints.expand(),
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
             
-            
-            new Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        left: 20.0, right: 20.0, top: 10.0),
-                    child: new Container(
-                        alignment: Alignment.center,
-                        height: 60.0,
-                        decoration: new BoxDecoration(
-                            color: Color(0xFF4364A1),
-                            borderRadius: new BorderRadius.circular(9.0)),
-                        child: new Text("Đăng nhập bằng FaceBook",
-                            style: new TextStyle(
-                                fontSize: 20.0, color: Colors.white))),
+            children: <Widget>[
+              SizedBox(
+                height: 140,
+              ),
+              //Image.asset('ic_car_green.png'),
+              new StakedIcons(),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 20, 0, 6),
+                child: Text(
+                  "Chào bạn!",
+                  style: TextStyle(fontSize: 22, color: Color(0xff333333)),
+                ),
+              ),
+              Text(
+                "Đăng nhập để sử dụng App",
+                style: TextStyle(fontSize: 16, color: Color(0xff606470)),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 40, 0, 20),
+                child: TextField(
+                  controller: _emailController,
+                  style: TextStyle(fontSize: 18, color: Colors.black),
+                  decoration: InputDecoration(
+                      labelText: "Email",
+                      prefixIcon: Container(
+                          width: 50, child: Image.asset("assets/images/ic_mail.png")),
+                      border: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xffCED0D2), width: 1),
+                          borderRadius: BorderRadius.all(Radius.circular(6)))),
+                ),
+              ),
+              TextField(
+                controller: _passController,
+                style: TextStyle(fontSize: 18, color: Colors.black),
+                obscureText: true,
+                decoration: InputDecoration(
+                    labelText: "Mật khẩu",
+                    prefixIcon: Container(
+                        width: 50, child: Image.asset("assets/images/ic_lock.png")),
+                    border: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Color(0xffCED0D2), width: 1),
+                        borderRadius: BorderRadius.all(Radius.circular(6)))),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 52,
+                  child: RaisedButton(
+                    onPressed: _onLoginClick,
+                    child: Text(
+                      "Đăng nhập",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+                    color: Color(0xff3277D8),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(6))),
                   ),
                 ),
-              ],
-            ),
-            
-            new Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        left: 20.0, right: 20.0, top: 10.0),
-                    child: new Container(
-                        alignment: Alignment.center,
-                        height: 60.0,
-                        decoration: new BoxDecoration(
-                            color: Color(0xFFDF513B),
-                            borderRadius: new BorderRadius.circular(9.0)),
-                        child: new Text("Đăng nhập bằng Google",
-                            style: new TextStyle(
-                                fontSize: 20.0, color: Colors.white))),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 15, 0, 0),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 52,
+                  child: RaisedButton(
+                    onPressed: _handleSignIn,
+                    child: Text(
+                      "Đăng nhập google",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+                    color: Color(0xFFDF513B),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(6))),
                   ),
-                )
-              ],
-            ),
-          ],
+                ),
+              ),
+             Padding(
+                padding: const EdgeInsets.fromLTRB(0, 15, 0, 100),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 52,
+                  child: RaisedButton(
+                    onPressed: _handleSignIn,
+                    child: Text(
+                      "Đăng nhập Facebook",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+                    color: Color(0xFF4364A1),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(6))),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                child: RichText(
+                  text: TextSpan(
+                      text: "Bạn là người mới? ",
+                      style: TextStyle(color: Color(0xff606470), fontSize: 16),
+                      children: <TextSpan>[
+                        TextSpan(
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => RegisterPage()));
+                              },
+                            text: "Đăng ký tài khoản ngay",
+                            style: TextStyle(
+                                color: Color(0xff3277D8), fontSize: 16))
+                      ]),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void _onLoginClick() {
+    String email = _emailController.text;
+    String pass = _passController.text;
+    var authBloc = MyApp.of(context).authBloc;
+    LoadingDialog.showLoadingDialog(context, "Loading...");
+    authBloc.signIn(email, pass, () {
+      LoadingDialog.hideLoadingDialog(context);
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => prefix0.Tab()));
+    }, (msg) {
+      LoadingDialog.hideLoadingDialog(context);
+      MsgDialog.showMsgDialog(context, "Lỗi đăng nhập", msg);
+    });
   }
 }
