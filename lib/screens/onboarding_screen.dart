7ddcd9d917984e5c9373247e1fb7b8909d64c230
Marutou.System <lhtrieu@mc-net.co.jp>
2020-06-27 00:14:27 +0700

    update assets

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -10,7 +10,6 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
-
 // Future checkFirstSeen() async {
 //     SharedPreferences prefs = await SharedPreferences.getInstance();
 //     bool _seen = (prefs.getBool('seen') ?? false);
@@ -61,6 +60,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     return Scaffold(
       body: AnnotatedRegion<SystemUiOverlayStyle>(
         value: SystemUiOverlayStyle.light,
+
         child: Container(
           decoration: BoxDecoration(
             gradient: LinearGradient(
@@ -83,10 +83,12 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                 Container(
                   alignment: Alignment.centerRight,
                   child: FlatButton(
-                    onPressed: (){
-                      Navigator.push(context, MaterialPageRoute(
-                           builder: (context) => prefix0.MainScreen(),
-                         ));
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => prefix0.MainScreen(),
+                          ));
                     },
                     child: Text(
                       'Bỏ qua',
@@ -110,83 +112,89 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                     children: <Widget>[
                       Padding(
                         padding: EdgeInsets.all(40.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Center(
-                              child: Image(
-                                image: AssetImage(
-                                  'assets/images/onboarding0.png',
+                        child: SingleChildScrollView(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Center(
+                                child: Image(
+                                  image: AssetImage(
+                                    'assets/images/onboarding0.png',
+                                  ),
+                                  height: 300.0,
+                                  width: 300.0,
                                 ),
-                                height: 300.0,
-                                width: 300.0,
                               ),
-                            ),
-                            SizedBox(height: 30.0),
-                            Text(
-                              'Ứng dụng\nnhận diện chữ viết tay',
-                              style: kTitleStyle,
-                            ),
-                            SizedBox(height: 15.0),
-                            Text(
-                              'Một trong những phương pháp học tiếng Nhật hiệu quả nhất đó là viết càng nhiều càng tốt',
-                              style: kSubtitleStyle,
-                            ),
-                          ],
+                              SizedBox(height: 30.0),
+                              Text(
+                                'Ứng dụng\nnhận diện chữ viết tay',
+                                style: kTitleStyle,
+                              ),
+                              SizedBox(height: 30.0),
+                              Text(
+                                'Một trong những phương pháp học tiếng Nhật hiệu quả nhất đó là viết càng nhiều càng tốt',
+                                style: kSubtitleStyle,
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                       Padding(
                         padding: EdgeInsets.all(40.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Center(
-                              child: Image(
-                                image: AssetImage(
-                                  'assets/images/onboarding1.png',
+                        child: SingleChildScrollView(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Center(
+                                child: Image(
+                                  image: AssetImage(
+                                    'assets/images/onboarding1.png',
+                                  ),
+                                  height: 300.0,
+                                  width: 300.0,
                                 ),
-                                height: 300.0,
-                                width: 300.0,
                               ),
-                            ),
-                            SizedBox(height: 30.0),
-                            Text(
-                              'Sử dụng mọi lúc mọi nơi\nmiễn phí!',
-                              style: kTitleStyle,
-                            ),
-                            SizedBox(height: 15.0),
-                            Text(
-                              'Dự án giúp người học tiếng Nhật có thể luyện tập viết tiếng Nhật qua những bài tập từ vỡ lòng đến sơ cấp, trung cấp và cao cấp.',
-                              style: kSubtitleStyle,
-                            ),
-                          ],
+                              SizedBox(height: 20.0),
+                              Text(
+                                'Sử dụng mọi lúc mọi nơi\nmiễn phí!',
+                                style: kTitleStyle,
+                              ),
+                              SizedBox(height: 10.0),
+                              Text(
+                                'Dự án giúp người học tiếng Nhật có thể luyện tập viết tiếng Nhật qua những bài tập từ vỡ lòng đến sơ cấp, trung cấp và cao cấp.',
+                                style: kSubtitleStyle,
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                       Padding(
                         padding: EdgeInsets.all(40.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Center(
-                              child: Image(
-                                image: AssetImage(
-                                  'assets/images/onboarding2.png',
+                        child: SingleChildScrollView(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Center(
+                                child: Image(
+                                  image: AssetImage(
+                                    'assets/images/onboarding2.png',
+                                  ),
+                                  height: 300.0,
+                                  width: 300.0,
                                 ),
-                                height: 300.0,
-                                width: 300.0,
                               ),
-                            ),
-                            SizedBox(height: 30.0),
-                            Text(
-                              'Đây là đồ án chuyên ngành',
-                              style: kTitleStyle,
-                            ),
-                            SizedBox(height: 15.0),
-                            Text(
-                              'Được làm bởi\nNgô Nhật Trí - Nguyễn Hoàng Phong ',
-                              style: kSubtitleStyle,
-                            ),
-                          ],
+                              SizedBox(height: 20.0),
+                              Text(
+                                'Đồ án tổng hợp',
+                                style: kTitleStyle,
+                              ),
+                              SizedBox(height: 10.0),
+                              Text(
+                                'Được làm bởi\nNgô Nhật Trí',
+                                style: kSubtitleStyle,
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ],
@@ -198,37 +206,37 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                 ),
                 _currentPage != _numPages - 1
                     ? Expanded(
-                        child: Align(
-                          alignment: FractionalOffset.bottomRight,
-                          child: FlatButton(
-                            onPressed: () {
-                              _pageController.nextPage(
-                                duration: Duration(milliseconds: 500),
-                                curve: Curves.ease,
-                              );
-                            },
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                Text(
-                                  'Tiếp theo',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 22.0,
-                                  ),
-                                ),
-                                SizedBox(width: 10.0),
-                                Icon(
-                                  Icons.arrow_forward,
-                                  color: Colors.white,
-                                  size: 30.0,
-                                ),
-                              ],
+                  child: Align(
+                    alignment: FractionalOffset.bottomRight,
+                    child: FlatButton(
+                      onPressed: () {
+                        _pageController.nextPage(
+                          duration: Duration(milliseconds: 500),
+                          curve: Curves.ease,
+                        );
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Text(
+                            'Tiếp',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 22.0,
                             ),
                           ),
-                        ),
-                      )
+                          SizedBox(width: 10.0),
+                          Icon(
+                            Icons.arrow_forward,
+                            color: Colors.white,
+                            size: 30.0,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                )
                     : Text(''),
               ],
             ),
@@ -237,30 +245,32 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
       ),
       bottomSheet: _currentPage == _numPages - 1
           ? Container(
-              height: 100.0,
-              width: double.infinity,
-              color: Colors.white,
-              child: GestureDetector(
-                onTap: () {
-                         Navigator.push(context, MaterialPageRoute(
-                           builder: (context) => LoginPage(),
-                         ));
-                      },
-                child: Center(
-                  child: Padding(
-                    padding: EdgeInsets.only(bottom: 30.0),
-                    child: Text(
-                      'Đăng nhập',
-                      style: TextStyle(
-                        color: Color(0xFF5B16D0),
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
+        height: 80.0,
+        width: double.infinity,
+        color: Colors.white,
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => LoginPage(),
+                ));
+          },
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 10.0),
+              child: Text(
+                'Đăng nhập',
+                style: TextStyle(
+                  color: Color(0xFF5B16D0),
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            )
+            ),
+          ),
+        ),
+      )
           : Text(''),
     );
   }
